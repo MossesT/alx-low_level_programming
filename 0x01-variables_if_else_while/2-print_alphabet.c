273a6ba43341521a969ba620195f49350fab60aa
mossest <tesfayemusse@gmail.com>
2022-09-12 17:04:59 -0700

    df git push

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 /**
  *  * main - main block
- *   * Description: Get a random number and check its last digit, compare it with 5
- *    * Return: 0
+ *   * Description: Get a random number and
+ *    * Return: 
  *     */
 int main(void)
 {
