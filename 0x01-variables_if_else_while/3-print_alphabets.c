641a5d08c614bd44d10697243087cb6ef9b3fcf8
mossest <tesfayemusse@gmail.com>
2022-09-12 17:12:35 -0700

    df

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,8 @@
  *   * Description:  prints the alphabet in lowercase,
  *    * and then in uppercase, followed by a new line.
  *     * Return: 0
- *      */
+ *
+      */
 int main(void)
 {
 char c = 'a';
