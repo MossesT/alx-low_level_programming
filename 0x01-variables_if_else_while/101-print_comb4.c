5e23ed5cab5ae13082e7353fbcf33c56c392105e
mossest <tesfayemusse@gmail.com>
2022-09-12 17:29:21 -0700

    df

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
  *    * different combinations of two digits
  *     * Numbers must be separated by ,, followed by a space
  *      * The two digits must be different
- *       * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
  *        * Print only the smallest combination of two digits
  *         * Numbers should be printed in ascending order, with two digits
  *          * You can only use the putchar function
@@ -15,7 +15,7 @@
  *             * You are not allowed to use any variable of type char
  *              * All your code should be in the main function
  *               * Return: 0
-                */
+ */
 int main(void)
 {
 int c;
