7aa399591cac1627d0a761397f54a49c2dbd3a57
mossest <tesfayemusse@gmail.com>
2022-09-20 13:00:50 -0700

    abc

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,20 @@
 #include "main.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * puts2 - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
  */
-int main(void)
-{
-char *str;
 
-str = "0123456789";
-puts2(str);
-return (0);
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n');
 }
