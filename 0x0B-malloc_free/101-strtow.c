74810974eae7aeb11c751a58184ca51683821884
mossest <tesfayemusse@gmail.com>
2022-10-05 19:46:14 -0700

    abc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,10 +9,10 @@
 
 int _strlen(char *s)
 {
-  int size = 0;
-  for (; s[size] != '\0'; size++)
-    ;
-  return (size);
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
 }
 
 /**
@@ -24,22 +24,22 @@ int _strlen(char *s)
 
 char *str_addChar (char *str, char c)
 {
-  int size, i;
-  char *m;
+int size, i;
+char *m;
 
-  size = _strlen(str);
+size = _strlen(str);
 
-  m = malloc((size + 1) * sizeof(char) + 1);
-  if (m == 0)
-    return (0);
+m = malloc((size + 1) * sizeof(char) + 1);
+if (m == 0)
+return (0);
 
-  for (i = 0; i <= size; i++)
-    m[i] = str[i];
+for (i = 0; i <= size; i++)
+m[i] = str[i];
 
-  m[i + 1] = c;
-  m[i + 2] = '\0';
+m[i + 1] = c;
+m[i + 2] = '\0';
 
-  return (m);
+return (m);
 }
 
 
@@ -53,13 +53,13 @@ unsigned int nbr_spaces(char *s)
 {
   int i, cmpt = 0;
 
-  for (i = 0; s[i + 1] != '\0'; i++)
-    {
-      if (s[i]  == ' ' && s[i + 1] != ' ')
-	cmpt++;
-    }
+for (i = 0; s[i + 1] != '\0'; i++)
+{
+if (s[i]  == ' ' && s[i + 1] != ' ')
+cmpt++;
+}
 
-  return (cmpt + 1);
+return (cmpt + 1);
 }
 
 
@@ -70,43 +70,43 @@ unsigned int nbr_spaces(char *s)
  */
 char **strtow(char *str)
 {
-  int i;
-  int spaces = nbr_spaces(str);
-  char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
-  char *token;
-  int checkingSpace = 0;
-  int word = 0;
+int i;
+int spaces = nbr_spaces(str);
+char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
+char *token;
+int checkingSpace = 0;
+int word = 0;
 
-  if (!tokens)
-    {
-      printf("Failed");
-      return (0);
+if (!tokens)
+{
+printf("Failed");
+return (0);
     }
 
 
-  printf("looping");
-  for (i = 0; str[i] != '\0'; i++)
-    {
-      if (str[i] == ' ')
+printf("looping");
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ')
+{
+if (checkingSpace == 0)
+{
+word++;
+checkingSpace = 1;
+} 
+}
+else
 	{
-	  if (checkingSpace == 0)
-	    {
-	      word++;
-	      checkingSpace = 1;
-	    } 
-	}
-      else
-	{
-	  printf("1");
-	  token = tokens[word];
-	  free(tokens[word]);
-	  str_addChar(token, str[i]);
-	  checkingSpace = 0;
-	}
+printf("1");
+token = tokens[word];
+free(tokens[word]);
+str_addChar(token, str[i]);
+checkingSpace = 0;
+}
 
-    }
+}
 
-  tokens[i] = NULL;
+tokens[i] = NULL;
 
-  return (tokens);
+return (tokens);
 }
